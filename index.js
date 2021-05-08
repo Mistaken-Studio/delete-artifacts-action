f82a9220c2d5683c23f49d77265a6ddb21687b57
grzes 1234 <g_sw@poczta.fm>
2021-05-08 11:51:22 +0200

    .

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const request = require("request");
+const { Octokit, App, Action } = require("octokit");
 
-try {
-  // `who-to-greet` input defined in action metadata file
+try 
+{
+  console.log(core);
   const leaveNewest = core.getInput('leaveNewest');
   const owner = core.getInput('owner');
   const repo = core.getInput('repo');
   console.log(`leaveNewest: ${leaveNewest}!`);
+  // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+  const octokit = new Octokit({ auth: `personal-access-token123` });
+
+  // Compare: https://docs.github.com/en/rest/reference/users#get-the-authenticated-user
+  const {
+    data: { login },
+  } = await octokit.users.getAuthenticated();
+  console.log("Hello, %s", login);
   request.get(`https://api.github.com/repos/${owner}/${repo}/actions/artifacts`, {
     headers: {
-    'User-Agent': 'request'
-  }}, (error, response, body) => {
+      'User-Agent': 'request'
+    }
+  }, (error, response, body) => {
     if(error) return core.setFailed(error);
     console.log(body);
   });
-} catch (error) {
+} 
+catch (error) 
+{
   core.setFailed(error.message);
 }
