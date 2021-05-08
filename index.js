596a5350a1ea113b0a53ddd3f1a03480223c2444
grzes 1234 <g_sw@poczta.fm>
2021-05-08 11:55:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,7 @@ try
   const owner = core.getInput('owner');
   const repo = core.getInput('repo');
   console.log(`leaveNewest: ${leaveNewest}!`);
-  // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
-  const octokit = new Octokit({ auth: `personal-access-token123` });
-
-  // Compare: https://docs.github.com/en/rest/reference/users#get-the-authenticated-user
-  const {
-    data: { login },
-  } = await octokit.users.getAuthenticated();
-  console.log("Hello, %s", login);
+  const octokit = github.getOctokit();
   request.get(`https://api.github.com/repos/${owner}/${repo}/actions/artifacts`, {
     headers: {
       'User-Agent': 'request'
