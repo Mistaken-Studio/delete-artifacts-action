8712bbc5244ee2a94633979d19edc7eccb96c40c
grzes 1234 <g_sw@poczta.fm>
2021-05-08 12:41:13 +0200

    .

--- index.js
+++ index.js
@@ -1,41 +1,51 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 (async () => {
-  try 
-  {
-    const leaveNewest = core.getInput('leaveNewest');
-    const owner = github.context.repo.owner;
-    const repo = github.context.repo.repo;
-    const octokit = github.getOctokit(core.getInput("repo-token"));
-    let artifacts = await octokit.actions.listArtifactsForRepo({repo: repo, owner: owner});
-    if(artifacts.data.total_count == 0) {
-      console.log("Nothing to delete");
-      return;
-    }
-    let latest = artifacts.data.artifacts[0];
-    if(leaveNewest) 
-    {
-      artifacts.data.artifacts.forEach(artifact => {
-        if(new Date(artifact.created_at) > new Date(latest.created_at)) latest = artifact;
-      });
-    }
-    else latest = null;
-    artifacts.data.artifacts.forEach(async artifact => {
-      if(latest == artifact) return;
-      console.log(`Deleting ${artifact.id}`);
-      try 
-      {
-        await octokit.actions.deleteArtifact({repo: repo, owner: owner, artifact_id: artifact.id});
-      } 
-      catch (error) 
-      {
-        core.setFailed(error.message);
-      }
-      console.log(`Deleted ${artifact.id}`);
-    });
-  } 
-  catch (error) 
-  {
-    core.setFailed(error.message);
-  }
+	try 
+	{
+		const leaveNewest = core.getInput('leaveNewest');
+		const owner = github.context.repo.owner;
+		const repo = github.context.repo.repo;
+		const octokit = github.getOctokit(core.getInput("repo-token"));
+		let artifacts = await octokit.actions.listArtifactsForRepo({repo: repo, owner: owner});
+		if(artifacts.data.total_count == 0) 
+		{
+			console.log("Nothing to delete");
+			return;
+		}
+		let latest = artifacts.data.artifacts[0];
+		let latestDate = new Date(latest.created_at);
+		if(leaveNewest) 
+		{
+			artifacts.data.artifacts.forEach(artifact => {
+			let date = new Date(artifact.created_at);
+			if(date > latestDate) 
+			{
+				latest = artifact;
+				latestDate = date;
+			}
+			});
+		}
+		else 
+			latest = null;
+		artifacts.data.artifacts.forEach(async artifact => {
+			if(latest == artifact) return;
+			console.log(`Deleting ${artifact.id}`);
+			try 
+			{
+				await octokit.actions.deleteArtifact({repo: repo, owner: owner, artifact_id: artifact.id});
+			} 
+			catch (error) 
+			{
+				core.setFailed(error.message);
+			}
+			console.log(`Deleted ${artifact.id}`);
+		});
+
+		console.log(`Done`);
+	} 
+	catch (error) 
+	{
+		core.setFailed(error.message);
+	}
 })();
