bbd652f04adb22f8892baa083d63f549d16ab3fc
grzes 1234 <g_sw@poczta.fm>
2021-05-08 12:09:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ const { Octokit, App, Action } = require("octokit");
     const leaveNewest = core.getInput('leaveNewest');
     const owner = github.context.repo.owner;
     const repo = github.context.repo.repo;
-    const octokit = github.getOctokit("GITHUB_TOKEN");
+    const octokit = github.getOctokit(core.getInput("repo-token"));
     let artifacts = await octokit.actions.listArtifactsForRepo({repo: repo, owner: owner});
     if(artifacts.data.total_count == 0) {
       console.log("Nothing to delete");
       return;
     }
     let latest = artifacts[0];
-    artifacts.data.artifacts.forEach(artifact => {
-      if(new Date(artifact.created_at) > new Date(latest.created_at)) latest = artifact;
-    });
+    if(leaveNewest) 
+    {
+      artifacts.data.artifacts.forEach(artifact => {
+        if(new Date(artifact.created_at) > new Date(latest.created_at)) latest = artifact;
+      });
+    }
+    else latest = null;
     artifacts.data.artifacts.forEach(async artifact => {
       if(latest == artifact) return;
       console.log(`Deleting ${artifact.id}`);
