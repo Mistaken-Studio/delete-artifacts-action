f8db1da23470a2602212045670a84781b4547c56
grzes 1234 <g_sw@poczta.fm>
2021-05-08 12:05:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,39 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { Octokit, App, Action } = require("octokit");
 (async () => {
-try 
-{
-  console.log(core);
-  const leaveNewest = core.getInput('leaveNewest');
-  const owner = core.getInput('owner');
-  const repo = core.getInput('repo');
-  console.log(`leaveNewest: ${leaveNewest}!`);
-  const octokit = github.getOctokit();
-  request.get(`https://api.github.com/repos/${owner}/${repo}/actions/artifacts`, {
-    headers: {
-      'User-Agent': 'request'
+  try 
+  {
+    console.log(core);
+    const leaveNewest = core.getInput('leaveNewest');
+    const owner = github.context.repo.owner;
+    const repo = github.context.repo.repo;
+    console.log(`leaveNewest: ${leaveNewest}!`);
+    const octokit = github.getOctokit(repo);
+    let artifacts = await octokit.actions.listArtifactsForRepo({repo: repo, owner: owner});
+    if(artifacts.data.total_count == 0) {
+      console.log("Nothing to delete");
+      return;
     }
-  }, (error, response, body) => {
-    if(error) return core.setFailed(error);
-    console.log(body);
-  });
-} 
-catch (error) 
-{
-  core.setFailed(error.message);
-}
+    let latest = artifacts[0];
+    artifacts.data.artifacts.forEach(artifact => {
+      if(new Date(artifact.created_at) > new Date(latest.created_at)) latest = artifact;
+    });
+    artifacts.data.artifacts.forEach(async artifact => {
+      if(latest == artifact) continue;
+      console.log(`Deleting ${artifact.id}`);
+      try 
+      {
+        await octokit.actions.deleteArtifact({repo: repo, owner: owner, artifact_id: artifact.id});
+      } 
+      catch (error) 
+      {
+        core.setFailed(error.message);
+      }
+      console.log(`Deleted ${artifact.id}`);
+    });
+  } 
+  catch (error) 
+  {
+    core.setFailed(error.message);
+  }
 })();
